a66bd975b3cc54014416eee07593286f12427891
Krishna Patel <48583952+kri25shna99@users.noreply.github.com>
2022-10-12 19:05:25 -0500

    Create maximumSubarray.py

---
+++ maximumSubarray.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #null condition
+        if len(nums)==0:
+            return -math.inf
+        if len(nums)==1:
+            return nums[0]
+        #consdering running sum senerio
+        """
+        currsum max
+        -2      -2
+        1       1
+        -2      1
+        4       4
+        3       4
+        5       5
+        6       6
+        1       6
+        5       6
+        """
+        maximum = nums[0]
+        currSum = nums[0]
+        for i in range(1,len(nums)):
+            currSum = max(nums[i], currSum+nums[i])
+            maximum = max(currSum,maximum)
+        return maximum
